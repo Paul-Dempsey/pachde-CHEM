7b5ed4729eecf1f8318f8c244437c7ee0031353c
Paul Dempsey <pcdempsey@live.com>
2025-05-08 12:52:37 -0700

    Make preset json easier to hack (e.g. for Osmose)

--- src/em/preset.cpp
+++ src/em/preset.cpp
@@ -4,28 +4,55 @@
 
 namespace pachde {
 
+std::string id_spec_to_string(PresetId id) {
+    return format_string("%d.%d.%d", id.bank_hi(), id.bank_lo(), id.number());
+}
+
+PresetId parse_id_spec(std::string spec)
+{
+    if (spec.empty()) return PresetId();
+
+    uint8_t hi = 0, lo = 0, n = 0;
+    auto it = spec.cbegin();
+    while (it != spec.cend() && ('.' != *it)) {
+        assert(isdigit(*it));
+        hi = (hi*10) + (*it - '0');
+        it++;
+    }
+    it++;
+    while (it != spec.cend() && ('.' != *it)) {
+        assert(isdigit(*it));
+        lo = (lo*10) + (*it - '0');
+        it++;
+    }
+    it++;
+    while (it != spec.cend() && ('.' != *it)) {
+        assert(isdigit(*it));
+        n = (n*10) + (*it - '0');
+        it++;
+    }
+    return PresetId(hi,lo,n);
+}
+
 json_t* PresetDescription::toJson(bool include_id, bool include_name, bool include_text)
 {
     json_t* root = json_object();
     if (include_id) {
-        json_object_set_new(root, "id", json_integer(id.key()));
+        json_object_set_new(root, "id", json_string(id_spec_to_string(id).c_str()));
     }
     if (include_name) {
-        json_object_set_new(root, "name", json_stringn(name.c_str(), name.size()));
+        json_object_set_new(root, "name", json_string(name.c_str()));
     }
     if (include_text && text.size()) {
         auto data = collapse_space(text);
-        json_object_set_new(root, "text", json_stringn(data.c_str(), data.size()));
+        json_object_set_new(root, "text", json_string(data.c_str()));
     }
     return root;
 }
 
 void PresetDescription::fromJson(const json_t* root)
 {
-    auto j = json_object_get(root, "id");
-    if (j) {
-        id = {uint32_t(json_integer_value(j))};
-    }
+    id = parse_id_spec(get_json_string(root, "id"));
     name = get_json_string(root, "name");
     text = get_json_string(root, "text");
 }
